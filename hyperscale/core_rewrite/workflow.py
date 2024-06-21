db58ec0efba1fa954dcedf591a63cb8edc5662db
Ada Lundhe <ada@datavant.com>
2024-06-21 01:43:40 +0000

    AL: refactor to remove optimizer

--- hyperscale/core_rewrite/workflow.py
+++ hyperscale/core_rewrite/workflow.py
@@ -1,5 +1,8 @@
+import threading
 import uuid
 from typing import Any, Dict
+
+from hyperscale.core_rewrite.snowflake.snowflake_generator import SnowflakeGenerator
 
 from .engines.client import Client
 from .hooks import Hook
@@ -9,7 +12,12 @@ class Workflow:
     def __init__(self):
         self.graph = __file__
         self.name = self.__class__.__name__
-        self.id = str(uuid.uuid4())
+
+        generator = SnowflakeGenerator(
+            (uuid.uuid1().int + threading.get_native_id()) >> 64
+        )
+
+        self.id = generator.generate()
 
         self.context: Dict[str, Any] = {}
         self.hooks: Dict[str, Hook] = {}
