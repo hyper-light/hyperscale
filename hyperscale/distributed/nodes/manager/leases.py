15272c18b0cff9d87cbb16f70dafefa2568eca70
Ada Lundhe <adalundhe@Adas-MacBook-Pro-2.local>
2026-01-13 17:59:38 -0600

    Auto-commit: 2026-01-13 17:59:38

--- hyperscale/distributed/nodes/manager/leases.py
+++ hyperscale/distributed/nodes/manager/leases.py
@@ -33,13 +33,13 @@ class ManagerLeaseCoordinator:
         config: "ManagerConfig",
         logger: "Logger",
         node_id: str,
-        task_runner,
+        task_runner: "TaskRunner",
     ) -> None:
-        self._state = state
-        self._config = config
-        self._logger = logger
-        self._node_id = node_id
-        self._task_runner = task_runner
+        self._state: "ManagerState" = state
+        self._config: "ManagerConfig" = config
+        self._logger: "Logger" = logger
+        self._node_id: str = node_id
+        self._task_runner: "TaskRunner" = task_runner
 
     def is_job_leader(self, job_id: str) -> bool:
         """
