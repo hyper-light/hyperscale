43a168dc635a560aa7f80b243b796d31f166d117
Ada Lundhe <adalundhe@Adas-MacBook-Pro-2.local>
2026-01-14 17:16:29 -0600

    Auto-commit: 2026-01-14 17:16:29

--- tests/framework/actions/default_registry.py
+++ tests/framework/actions/default_registry.py
@@ -1,6 +1,8 @@
 from tests.framework.actions.action_registry import ActionRegistry
+from tests.framework.actions.assert_condition import run as assert_condition
 from tests.framework.actions.await_gate_leader import run as await_gate_leader
 from tests.framework.actions.await_job import run as await_job
+from tests.framework.actions.await_manager_leader import run as await_manager_leader
 from tests.framework.actions.restart_nodes import run as restart_nodes
 from tests.framework.actions.sleep_action import run as sleep_action
 from tests.framework.actions.start_cluster import run as start_cluster
@@ -14,6 +16,8 @@ def build_default_registry() -> ActionRegistry:
     registry.register("start_cluster", start_cluster)
     registry.register("stop_cluster", stop_cluster)
     registry.register("await_gate_leader", await_gate_leader)
+    registry.register("await_manager_leader", await_manager_leader)
+    registry.register("assert_condition", assert_condition)
     registry.register("sleep", sleep_action)
     registry.register("submit_job", submit_job)
     registry.register("await_job", await_job)
