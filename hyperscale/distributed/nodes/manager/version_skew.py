539b5c7c66c38749f3b3c97db69e644d17b3183b
Ada Lundhe <adalundhe@Adas-MacBook-Pro-2.local>
2026-01-13 18:05:36 -0600

    Auto-commit: 2026-01-13 18:05:36

--- hyperscale/distributed/nodes/manager/version_skew.py
+++ hyperscale/distributed/nodes/manager/version_skew.py
@@ -47,13 +47,13 @@ class ManagerVersionSkewHandler:
         config: "ManagerConfig",
         logger: "Logger",
         node_id: str,
-        task_runner,
+        task_runner: "TaskRunner",
     ) -> None:
-        self._state = state
-        self._config = config
-        self._logger = logger
-        self._node_id = node_id
-        self._task_runner = task_runner
+        self._state: "ManagerState" = state
+        self._config: "ManagerConfig" = config
+        self._logger: "Logger" = logger
+        self._node_id: str = node_id
+        self._task_runner: "TaskRunner" = task_runner
 
         # Our capabilities
         self._local_capabilities = NodeCapabilities.current(
