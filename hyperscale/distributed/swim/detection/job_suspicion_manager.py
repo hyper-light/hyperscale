e353352ed7c9ad5f21984f28af07bda0f673deef
Ada Lundhe <adalundhe@Adas-MacBook-Pro-2.local>
2026-01-14 15:39:18 -0600

    Auto-commit: 2026-01-14 15:39:18

--- hyperscale/distributed/swim/detection/job_suspicion_manager.py
+++ hyperscale/distributed/swim/detection/job_suspicion_manager.py
@@ -18,6 +18,8 @@ import time
 from dataclasses import dataclass, field
 from typing import Callable
 
+from hyperscale.distributed.swim.core.protocols import LoggerProtocol
+
 from .suspicion_state import SuspicionState
 
 
