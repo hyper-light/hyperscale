f0b8f7a4defd104dfd77968e16a0e1822227f325
Ada Lundhe <adalundhe@Adas-MacBook-Pro-2.local>
2026-01-14 18:13:14 -0600

    Auto-commit: 2026-01-14 18:13:14

--- tests/framework/runtime/workflow_factory.py
+++ tests/framework/runtime/workflow_factory.py
@@ -1,6 +1,6 @@
 import importlib
 import inspect
-from typing import Any
+from typing import Any, Awaitable, Callable, cast
 
 from hyperscale.core.graph.workflow import Workflow
 from hyperscale.core.hooks.step import step
